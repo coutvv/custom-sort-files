e3f4f79f723505dd71655831ccf970ab3aeb28d9
Roman Lomovtsev <coutvv@gmail.com>
2025-02-23 20:23:03 +0300

    make README always last

--- src/main/kotlin/com/lomovtsev/customsortfiles/ZFirstProjectViewPane.kt
+++ src/main/kotlin/com/lomovtsev/customsortfiles/ZFirstProjectViewPane.kt
@@ -5,10 +5,8 @@ import com.intellij.ide.impl.ProjectViewSelectInPaneTarget
 import com.intellij.ide.projectView.impl.ProjectAbstractTreeStructureBase
 import com.intellij.ide.projectView.impl.ProjectViewPane
 import com.intellij.ide.util.treeView.NodeDescriptor
-import com.intellij.openapi.actionSystem.DefaultActionGroup
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import javax.swing.JComponent
 
 class ZFirstProjectViewPane(project: Project): ProjectViewPane(project) {
     companion object {
@@ -16,15 +14,12 @@ class ZFirstProjectViewPane(project: Project): ProjectViewPane(project) {
     }
 
     private val logger = thisLogger()
+    private val filename = "README.md"
 
     override fun getTitle(): String = "ZFirst View"
 
     override fun getId(): String = ID
     override fun getWeight(): Int = 50
-
-    override fun addToolbarActions(actionGroup: DefaultActionGroup) {
-        super.addToolbarActions(actionGroup)
-    }
 
     override fun createStructure(): ProjectAbstractTreeStructureBase {
         return ZFirstTreeStructure(this, myProject)
@@ -35,14 +30,8 @@ class ZFirstProjectViewPane(project: Project): ProjectViewPane(project) {
     }
     
 
-    override fun createComparator(): java.util.Comparator<NodeDescriptor<*>?> {
-        val comparator = super.createComparator();
-        return Comparator { o1, o2 ->
-            logger.warn("sorting comparator for $o1 and $o2")
-            if (o1.toString() == "zfirst.txt") { return@Comparator -1 }
-            if (o2.toString() == "zfirst.txt") { return@Comparator 1 }
-            return@Comparator comparator.compare(o1, o2)
-        }
+    override fun createComparator(): Comparator<NodeDescriptor<*>?> {
+        return readmeLastComparator()
     }
     
 
@@ -54,6 +43,26 @@ class ZFirstProjectViewPane(project: Project): ProjectViewPane(project) {
     override fun createSelectInTarget(): SelectInTarget {
         return ProjectViewSelectInPaneTarget(myProject, this, false)
     }
-
+    
+    private fun readmeLastComparator(): Comparator<NodeDescriptor<*>?> {
+        val comparator = super.createComparator();
+        return Comparator { o1, o2 ->
+            logger.warn("sorting comparator for $o1 and $o2")
+            if (o1.toString() == filename) { return@Comparator 1 }
+            if (o2.toString() == filename) { return@Comparator -1 }
+            return@Comparator comparator.compare(o1, o2)
+        }
+    }
+    
+     
+    private fun zFirstComparator(): Comparator<NodeDescriptor<*>?> {
+        val comparator = super.createComparator();
+        return Comparator { o1, o2 ->
+            logger.warn("sorting comparator for $o1 and $o2")
+            if (o1.toString() == "zfirst.txt") { return@Comparator -1 }
+            if (o2.toString() == "zfirst.txt") { return@Comparator 1 }
+            return@Comparator comparator.compare(o1, o2)
+        }
+    }
 
 }
