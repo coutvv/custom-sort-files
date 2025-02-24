affd177dfc1ec2c080b407b43e0af8492b6c1297
Roman Lomovtsev <coutvv@gmail.com>
2025-02-24 15:52:41 +0300

    cleanup

--- src/main/kotlin/com/lomovtsev/customsortfiles/ZFirstProjectViewPane.kt
+++ src/main/kotlin/com/lomovtsev/customsortfiles/ZFirstProjectViewPane.kt
@@ -1,61 +1,25 @@
 package com.lomovtsev.customsortfiles
 
-import com.intellij.ide.SelectInTarget
-import com.intellij.ide.impl.ProjectViewSelectInPaneTarget
-import com.intellij.ide.projectView.impl.ProjectAbstractTreeStructureBase
 import com.intellij.ide.projectView.impl.ProjectViewPane
-import com.intellij.ide.projectView.impl.ProjectViewTree
 import com.intellij.ide.util.treeView.NodeDescriptor
-import com.intellij.openapi.actionSystem.DefaultActionGroup
-import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import java.util.Comparator
-import javax.swing.tree.DefaultTreeModel
 
 class ZFirstProjectViewPane(project: Project) : ProjectViewPane(project) {
     companion object {
         const val ID = "ZFirstProjectView"
     }
 
-    private val logger = thisLogger()
-    private val filename = "README.md"
-
     override fun getTitle(): String = "ZFirst View"
 
     override fun getId(): String = ID
     override fun getWeight(): Int = 50
 
+    /**
+     * break the ordering after StructureProvider modifications 
+     */
     override fun createComparator(): Comparator<NodeDescriptor<*>> {
         return Comparator { _, _ -> 0 }
     }
 
-    private fun readmeLastComparator(): Comparator<NodeDescriptor<*>?> {
-        val comparator = super.createComparator();
-        return Comparator { o1, o2 ->
-            logger.warn("sorting comparator for $o1 and $o2")
-            if (o1.toString() == filename) {
-                return@Comparator 1
-            }
-            if (o2.toString() == filename) {
-                return@Comparator -1
-            }
-            return@Comparator comparator.compare(o1, o2)
-        }
-    }
-
-
-    private fun zFirstComparator(): Comparator<NodeDescriptor<*>?> {
-        val comparator = super.createComparator();
-        return Comparator { o1, o2 ->
-            logger.warn("sorting comparator for $o1 and $o2")
-            if (o1.toString() == "zfirst.txt") {
-                return@Comparator -1
-            }
-            if (o2.toString() == "zfirst.txt") {
-                return@Comparator 1
-            }
-            return@Comparator comparator.compare(o1, o2)
-        }
-    }
 
 }
--- src/main/kotlin/com/lomovtsev/customsortfiles/ZFirstTreeStructureProvider.kt
+++ src/main/kotlin/com/lomovtsev/customsortfiles/ZFirstTreeStructureProvider.kt
@@ -2,16 +2,13 @@ package com.lomovtsev.customsortfiles
 
 import com.intellij.ide.projectView.TreeStructureProvider
 import com.intellij.ide.projectView.ViewSettings
-import com.intellij.ide.projectView.impl.GroupByTypeComparator
 import com.intellij.ide.projectView.impl.nodes.PsiFileNode
 import com.intellij.ide.util.treeView.AbstractTreeNode
-import com.intellij.ide.util.treeView.NodeDescriptor
 import com.intellij.openapi.project.Project
 
 class ZFirstTreeStructureProvider(
     private val project: Project
 ) : TreeStructureProvider {
-    val comparator: Comparator<NodeDescriptor<*>> = GroupByTypeComparator(project, "zFirstPaneId")
     val comparatorV2 = CustomGroupByTypeComparator()
     override fun modify(
         parent: AbstractTreeNode<*>,
@@ -25,17 +22,13 @@ class ZFirstTreeStructureProvider(
             return@find false
         }
 
-//        val result = ArrayList<AbstractTreeNode<*>>()
         val result = children.filter { it != zFirst }
             .sortedWith(comparatorV2)
             .toMutableList()
-//            .forEach { result.add(it) }
+        
         zFirst?.also { 
             result.add(1, it) // TODO: fix it!
         }
-
-//        children.clear()
-//        children.addAll(result)
 
         return result
     }
