97873e3dec68cd6dbc23d473f4717221ffe66dbb
Roman Lomovtsev <coutvv@gmail.com>
2025-02-24 16:23:57 +0300

    add fileOrderStorage & move state of sorting there

--- src/main/kotlin/com/lomovtsev/customsortfiles/CustomSortTreeStructureProvider.kt
+++ src/main/kotlin/com/lomovtsev/customsortfiles/CustomSortTreeStructureProvider.kt
@@ -1,5 +1,7 @@
 package com.lomovtsev.customsortfiles
 
+import ai.grazie.utils.LinkedSet
+import ai.grazie.utils.toLinkedSet
 import com.intellij.ide.projectView.TreeStructureProvider
 import com.intellij.ide.projectView.ViewSettings
 import com.intellij.ide.projectView.impl.nodes.PsiFileNode
@@ -7,30 +9,50 @@ import com.intellij.ide.util.treeView.AbstractTreeNode
 import com.intellij.openapi.project.Project
 
 class CustomSortTreeStructureProvider(
-    private val project: Project
+    project: Project
 ) : TreeStructureProvider {
+    val fileOrderStorage =  FileOrderStorage.getInstance(project)
+    
     val comparatorV2 = CustomGroupByTypeComparator()
     override fun modify(
         parent: AbstractTreeNode<*>,
         children: MutableCollection<AbstractTreeNode<*>>,
         settings: ViewSettings?
     ): MutableCollection<AbstractTreeNode<*>> {
-        val zFirst = children.find { node ->
-            if (node is PsiFileNode) {
-                return@find node.value.name == "zfirst.txt"
-            }
-            return@find false
-        }
+        
+        val customPositions = fileOrderStorage.state.customFilePositions
+        
+        val customs = findCustom(children, customPositions)
 
-        val result = children.filter { it != zFirst }
+        val result = children
+            .filter { !customs.contains(it) }
             .sortedWith(comparatorV2)
             .toMutableList()
         
-        zFirst?.also { 
-            result.add(1, it) // TODO: fix it!
+        customs.forEach {
+            val name = it.filename()
+            val index = customPositions[name]!!
+            result.add(index, it)
         }
 
         return result
     }
+    
+    private fun findCustom(
+        children: Collection<AbstractTreeNode<*>>, 
+        customPositions: Map<String, Int>
+    ): LinkedSet<AbstractTreeNode<*>> {
+        return children.filter { it is PsiFileNode && customPositions.containsKey(it.value.name) }
+            .map {  it as PsiFileNode }
+            .sortedBy { customPositions[it.value.name] }
+            .toLinkedSet()
+    }
 
+}
+
+private fun AbstractTreeNode<*>.filename(): String? {
+    if  (this is PsiFileNode) {
+        return this.value.name
+    }
+    return null;
 }
---
+++ src/main/kotlin/com/lomovtsev/customsortfiles/FileOrderStorage.kt
@@ -0,0 +1,38 @@
+package com.lomovtsev.customsortfiles
+
+import com.intellij.openapi.components.PersistentStateComponent
+import com.intellij.openapi.components.Service
+import com.intellij.openapi.components.State
+import com.intellij.openapi.components.Storage
+import com.intellij.openapi.project.Project
+
+
+@State(
+    name = "FileOrderStorage",
+    storages = [Storage("custom-sort-files.xml")]
+)
+@Service(Service.Level.PROJECT)
+class FileOrderStorage(
+    private var state: State = State(mutableMapOf(
+        "zfirst.txt" to 1,
+        "README.md" to 0, // TODO: think about last index as -1
+    ))
+) : PersistentStateComponent<FileOrderStorage.State> {
+    companion object {
+        fun getInstance(project: Project): FileOrderStorage {
+            return project.getService(FileOrderStorage::class.java)
+        }
+    }
+    
+    override fun getState(): State {
+        return state
+    }
+
+    override fun loadState(state: State) {
+        this.state = state
+    }
+
+    data class State(
+        val customFilePositions: MutableMap<String, Int> = mutableMapOf(),
+    )
+}
